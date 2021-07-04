fe653c02cc476fd8086a69e5805b5eef2f12fc12
PmaiWoW <PmaiWoW@Gmail.com>
2021-07-04 02:34:33 +0100

    Fix forms' text color

--- website/forms.py
+++ website/forms.py
@@ -11,11 +11,21 @@ class AgentForm(forms.ModelForm):
     def emailToString(self):
         return str(self.fields['email'])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'style': 'color: black'})
+
 
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'style': 'color: black'})
 
 
 class QuizForm(forms.ModelForm):
@@ -24,7 +34,17 @@ class QuizForm(forms.ModelForm):
         fields = '__all__'
         exclude = ['correctAnswers']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'style': 'color: black'})
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'style': 'color: black'})
